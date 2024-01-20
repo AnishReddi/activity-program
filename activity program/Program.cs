1a19148e7f76798956404bf1da98e1b4af21e13b
imana <imana@DESKTOP-ICTLH9C>
2024-01-19 20:34:31 -0500

    Add project files.

---
+++ activity program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace CSharpFundamentalsLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your First Name:");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Your Last Name:");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Your Student ID:");
+            string id = Console.ReadLine();
+
+            string result = $"{firstname[0]}.{lastname}{id.Substring(5, 4)}@conestogac.on.ca";
+            Console.WriteLine(result.ToLower());
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
